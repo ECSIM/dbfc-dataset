573778ea0b3ba5c0367d6a96a4d6ebf131af5ef5
sadrasabouri <sabouri.sadra@gmail.com>
2020-04-10 11:43:06 +0430

    add : plot_func added to function.py

---
+++ Notebooks/function.py
@@ -0,0 +1,51 @@
+import matplotlib.pyplot as plt
+
+def plot_func(
+        x,
+        y,
+        title,
+        x_label,
+        y_label,
+        color='green',
+        legend=[],
+        marker=[],
+        linewidth=3,
+        multi=False):
+    """
+    Plot function.
+
+    :param x: x-axis data
+    :type x: list or numpy array
+    :param y: y-axis data
+    :type y: list or numpy array
+    :param title: plot title
+    :type title: str
+    :param x_label: x-axis label
+    :type x_label: str
+    :param y_label: y-axis label
+    :type y_label: str
+    :param color: plot color
+    :type color: str or list
+    :param legend: plot legends
+    :type legend: list
+    :param marker : data marker
+    :type marker: list
+    :param linewidth: plot line width
+    :type linewidth: int
+    :param multi: multi plot flag
+    :type multi: bool
+    :return: None
+    """
+    plt.figure()
+    plt.grid()
+    if multi:
+        for index, y_item in enumerate(y):
+            plt.plot(x[index], y_item, color=color[index], marker=marker[index], linewidth=linewidth)
+    else:
+        plt.plot(x, y, color=color, linewidth=linewidth)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.title(title)
+    if len(legend) != 0:
+        plt.legend(legend)
+    plt.show()
